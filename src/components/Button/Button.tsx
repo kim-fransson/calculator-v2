b20588ca7a21c988181c9e7752558099d811779d
Kim Fransson <kim.fransson.22@gmail.com>
2026-03-30 10:34:28 +0200

    Added nice 3D effect for the buttons

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import styles from "./Button.module.css";
+import clsx from "clsx";
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   variant: "type-1" | "type-2" | "type-3";
@@ -8,8 +9,13 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 
 function Button({ variant, children, ...props }: ButtonProps) {
   return (
-    <button className={styles[variant]} {...props}>
-      {children}
+    <button
+      className={clsx(styles.button, styles[variant])}
+      {...props}
+    >
+      <span className={styles.shadow}></span>
+      <span className={styles.edge}></span>
+      <span className={styles.front}>{children}</span>
     </button>
   );
 }
